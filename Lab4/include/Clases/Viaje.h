f4f4be581a832cbc6793191dc825e0ab27316616
fausb12 <faus.britos@hotmail.com>
2026-06-03 11:02:57 -0300

    Update Viaje.h

--- Lab4/include/Clases/Viaje.h
+++ Lab4/include/Clases/Viaje.h
@@ -16,6 +16,11 @@ private:
 public:
     Viaje(int codigo, DTFecha fecha, std::string origen, std::string destino, int asientosPublicados, float precio);
     ~Viaje();
+    bool cumpleCreiterio(DTFecha fecha, std::string origen, std::string destino, int asientos);
+    DTVehiculo getVehiculo();
+    DTDetalleReserva getReservas();
+    int obtenerCodigo();
+    DTFecha getFecha();
 };
 
 #endif
