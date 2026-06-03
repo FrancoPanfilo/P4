d5f36b3d1849b1773107e4f330fc758203396771
FrancoPanfilo <108556532+FrancoPanfilo@users.noreply.github.com>
2026-06-03 09:55:44 -0300

    cambios paco

--- Lab4/src/Viaje.cpp
+++ Lab4/src/Viaje.cpp
@@ -1,6 +1,7 @@
 #include "../include/Viaje.h"
 
-Viaje::Viaje(int codigo, DTFecha fecha, std::string origen, std::string destino, int asientosPublicados, float precio) {
+Viaje::Viaje(int codigo, DTFecha fecha, std::string origen, std::string destino, int asientosPublicados, float precio)
+{
     this->codigo = codigo;
     this->fecha = fecha;
     this->origen = origen;
