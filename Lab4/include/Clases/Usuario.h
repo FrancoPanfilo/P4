c81c677336532ce96f550ff8a570cf22cfa9a7c6
fran-martinez16 <francisco.martinez.sellanes@gmail.com>
2026-06-03 10:36:44 -0300

    Update Usuario.h

--- Lab4/include/Clases/Usuario.h
+++ Lab4/include/Clases/Usuario.h
@@ -7,12 +7,16 @@ class Usuario {
 protected:
     std::string nickname;
     std::string nombre;
-    std::string contrasena;
     std::string email;
+    std::string contrasena;
 
 public:
     Usuario(std::string nickname, std::string nombre, std::string contrasena, std::string email);
     virtual ~Usuario();
+
+    getNickname();
+    getNombre();
+    getEmail();
 };
 
 #endif
